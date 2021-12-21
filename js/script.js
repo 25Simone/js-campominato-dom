a004c1256c2a6ea947c84bf9d37ad7f13df45026
25Simone <dantsimone111@gmail.com>
2021-12-21 18:50:05 +0100

    codice pulito e revisionato, fix bottone e posizione delle bombe nel terzo livello di difficoltà

--- js/script.js
+++ js/script.js
@@ -1,105 +1,93 @@
-// functions
-// funzione evento cambio colore al click del box
-let contatore=0;
-function createEvent(element, i, maxCell) {
-    element.addEventListener('click', function() {
-        if(uniqueRandomList.includes(i)){
-            this.classList.add('red');
-            page.classList.add('op');
-            loseMessage.classList.remove('d-none')
-        } else {
-            this.classList.add('box-bg-color');
-            contatore++;
-            win(maxCell);
-        }
-    })
-}
-// definisco gli eventi legati alla vittoria della partita
-function win (maxCell) {
-    if(contatore === maxCell - 16) {
-        page.classList.add('op');
-        winMessage.classList.remove('d-none')
-    }
-}
-
-// funzione per creare i box collegata all'evento
-function createNewBox(container, i, maxCell){
-    const square = document.createElement('div');
-    square.className = 'box';
-    container.append(square); 
-    createEvent(square, i, maxCell);
-}
-
-// funzione per definire il numero di massimo di celle nella griglia
-function createGrid(maxCell) {
-    boxContainer.innerHTML = '';
-    uniqueRandomList = [];
-    for (let i = 0; i < maxCell; i++) {
-        createNewBox(boxContainer, i, maxCell);
-    }
-}
+// funzione per generare un numero random
+getRandomNumber = (min, max) => Math.floor(Math.random() * ((max + 1) - min)) + min; 
 
 // funzione che definisce la posizione delle mine nela griglia
 function bombPosition (maxCell) {
-    // genero 16 numeri random univoci che inserisco poi nell'array uniqueRandomList
-    while (uniqueRandomList.length < 16) {
-        const randomNum = getNumber(1, maxCell);
-        if(!uniqueRandomList.includes(randomNum)) {
-            uniqueRandomList.push(randomNum);
+    const bombsList = [];
+    while (bombsList.length < 16) {
+        const n = getRandomNumber(1, maxCell);
+        if(!bombsList.includes(n)) {
+            bombsList.push(n);
         }
+    }
+    return bombsList;
+}
+
+// funzione che descrive i possibili scenari a gioco terminato
+function endGame(isWin, score){
+    const loseMessage = document.getElementById('lose-message');
+    const winMessage = document.getElementById('win-message');
+    const punteggio = document.getElementById('score');
+    const page = document.querySelector('.page');
+    if (isWin, score) {
+        page.classList.add('op');
+        loseMessage.classList.remove('d-none');
+        refreshButton.classList.remove('d-none');
+        punteggio.classList.remove('d-none');
+        punteggio.innerHTML = score;
+    } else {
+        page.classList.add('op');
+        winMessage.classList.remove('d-none');
+        refreshButton.classList.remove('d-none');
+        punteggio.classList.remove('d-none');
+        punteggio.innerHTML = score;
     }
 }
 
-// funzione per generare un numero random
-function getNumber (min, max) {
-    const result = Math.floor(Math.random() * ((max + 1) - min)) + min;
-    return result;
+// funzione che descrive il funzionamento del gioco
+function game(maxCell, container, bombsPositionFunction) {
+    // definisco un contatore per il numero di box azzurri cliccati
+    let contatore = 0;
+    const bombs = bombsPositionFunction(maxCell);
+    // reset
+    container.innerHTML = '';
+    for(let i = 1; i <= maxCell; i++) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        container.append(box);
+
+        // definisco l'evento al click di ciascun box
+        box.addEventListener('click', () => {
+            if(bombs.includes(i)) {
+                box.classList.add('red');
+                endGame(false, contatore)
+            } else {
+                box.classList.add('box-bg-color')
+                contatore++;
+
+                if(contatore == maxCell - 16) {
+                    endGame(true, contatore);
+                }
+            }
+        })
+    }
 }
 
 
-
 // definisco la variabile di collegamento all'html
-const boxContainer = document.querySelector('.container');
-const page = document.querySelector('.page');
+const boxContainer = document.getElementById('container');
 
 // definisco le variabili dei tre bottoni
 const button1 = document.getElementById('button-level1');
 const button2 = document.getElementById('button-level2');
 const button3 = document.getElementById('button-level3');
 
-// definisco la variabile dei collegamento all'messaggio lose and win
-const loseMessage = document.querySelector('.lose-message');
-const winMessage = document.querySelector('.win-message');
-
 // definisco la varibile del bottone refresh
-const refreshButton = document.querySelector('.refresh-button');
+const refreshButton = document.getElementById('refresh-button');
 
-// dichiaro l'array della lista numeri
-let uniqueRandomList = [];
+// definisco l'evento al click del bottone refresh
+refreshButton.addEventListener('click', () => window.location.reload())
 
 // definisco gli eventi click dei tre bottoni
 button1.addEventListener('click', function() {
-    contatore = 0;
-    createGrid(100);
-    bombPosition(100);
-    boxContainer.classList.add('level1');
-    boxContainer.classList.remove('level2');
-    boxContainer.classList.remove('level3');
+    boxContainer.className = 'level1';
+    game(100, boxContainer, bombPosition);
 })
 button2.addEventListener('click', function() {
-    createGrid(81);
-    bombPosition(81);
-    boxContainer.classList.add('level2');
-    boxContainer.classList.remove('level1');
-    boxContainer.classList.remove('level3');
+    boxContainer.className = 'level2';
+    game(81, boxContainer, bombPosition);
 })
 button3.addEventListener('click', function() {
-    createGrid(49);
-    boxContainer.classList.add('level3');
-    boxContainer.classList.remove('level1');
-    boxContainer.classList.remove('level2');
+    boxContainer.className = 'level3';
+    game(49, boxContainer, bombPosition);
 })
-
-// definisco l'evento al click del bottone refresh
-refreshButton.addEventListener('click', 
-() => window.location.reload())
